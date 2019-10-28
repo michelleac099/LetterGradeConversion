95eb9d5350c5e44f7b43ff542d048730045c2407
Michelle Chis <s1230292@monmouth.edu>
2019-10-28 19:43:08 -0400

    Initial commit

---
+++ LetterGradeConversion/src/LetterGradeConversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class LetterGradeConversion {
+
+	public static void main(String[] args) {
+		
+		//Program for converting a letter grade to a numeric value.
+		Scanner in = new Scanner(System.in);
+
+	}
+
+}
